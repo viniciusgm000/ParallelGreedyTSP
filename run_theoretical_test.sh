a8c612e62772fb6e20d41ba938b7db16b815593d
Vinicius Gabriel Machado <vgm18@inf.ufpr.br>
2023-10-07 14:56:17 -0300

    Measured time dedicated to the sequential part of the algorithms

--- run_theoretical_test.sh
+++ run_theoretical_test.sh
@@ -13,14 +13,14 @@ rm $OUTPUT_OUTPUT_DIR/*
 
 make theoretical
 
-# for i in 1 2 3 4 5 6 7 8 9 10; do
-#    for e in $EXECUTIONS; do
-#        for c in $CITIES; do
-#            echo $i"_seq_"$c"_cities_"$e"_executions"
-#            taskset --cpu-list 0 ./tsp_seq < $INPUT_DIR/$c"_cities_"$e"_executions.in" > $OUTPUT_OUTPUT_DIR/$i"_seq_"$c"_cities_"$e"_executions_output.out"
-#        done
-#    done
-# done
+for i in 1 2 3 4 5 6 7 8 9 10; do
+   for e in $EXECUTIONS; do
+       for c in $CITIES; do
+           echo $i"_seq_"$c"_cities_"$e"_executions"
+           taskset --cpu-list 0 ./tsp_seq < $INPUT_DIR/$c"_cities_"$e"_executions.in" > $OUTPUT_OUTPUT_DIR/$i"_seq_"$c"_cities_"$e"_executions_output.out"
+       done
+   done
+done
 
 export OMP_PROC_BIND=spread
 
